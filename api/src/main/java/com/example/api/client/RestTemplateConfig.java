b2029e410f4887983795c13deb67d7f05a64e1f8
matheusned15 <matheusned15@gmail.com>
2025-05-23 14:12:42 -0300

    Adding messaging and DTOs

---
+++ api/src/main/java/com/example/api/client/RestTemplateConfig.java
@@ -0,0 +1,23 @@
+package com.example.api.client;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofMillis(300_000))
+                .readTimeout(Duration.ofMillis(300_000))
+                .build();
+    }
+
+
+}
+
